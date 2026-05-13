14cbf31278bce680c2fcec498660484bdcfa23eb
Logeswari960 <logeswari.m2024aids@sece.ac.in>
2026-05-13 10:25:13 +0530

    Update App.jsx

--- react-profile-main/src/App.jsx
+++ react-profile-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>logeswari M</h1>
+          <h1>logeswarim</h1>
           <p>
           <code>CI CD</code>
           </p>
